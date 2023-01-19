aa33dbf38d59721e3ebd8e35c4290fb12e8d32b7
Sergio Molina <sergio.molina_cardin.ext@nokia.com>
2023-01-19 10:41:47 +0100

    Code formatting

--- Reco_Facial.py
+++ Reco_Facial.py
@@ -1,4 +1,3 @@
-
 from asyncio.windows_events import NULL
 import cv2
 import numpy as np
@@ -8,155 +7,141 @@ from tkinter import messagebox
 from pathlib import Path
 import lock
 
+# Para instalar el m贸dulo tkinter usar uno de los siguiente comandos:
+#   pip3 install tkinter
+#   py -m pip install tkinter
 
-#para instalar el modulo tkinter simplemente:
-#pip3 install tkinter
-# o bien py -m pip install tkinter
 
-# variables globales
+
+# Variables globales
 # ------------------
-props_dict={}
+props_dict = {}
+
 
 def init(props):
     global props_dict
     print("Python: Enter in init")
-    
-    #props es un diccionario
-    props_dict= props
-    resultado=executeChallenge()
+
+    # Props es un diccionario
+    props_dict = props
+    resultado = executeChallenge()
     if (resultado[1]>0):
         return 0
-    else: 
+    else:
         return -1
-    
 
-           
+
+
 def executeChallenge():
     print("Starting execute")
-    #for key in os.environ: print(key,':',os.environ[key])
-    dataPath=os.environ['SECUREMIRROR_CAPTURES']
+    #for key in os.environ: print(key, ':', os.environ[key])
+    dataPath = os.environ['SECUREMIRROR_CAPTURES']
 
     print ("storage folder is :",dataPath)
-    
-    # mecanismo de lock BEGIN
+
+    # Mecanismo de lock BEGIN
     # -----------------------
     lock.lockIN("Reco_Facial")
 
 
     #dataPath = 'B:/Doctorado/Challenges/Data' #Cambia a la ruta donde hayas almacenado Data
-    imagePaths = os.listdir(dataPath+ "/" + "Data")
-    print('imagePaths=',imagePaths)
-    
+    imagePaths = os.listdir(dataPath + "/" + "Data")
+    print('imagePaths=', imagePaths)
+
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-    
+
     # Leyendo el modelo
     face_recognizer.read(dataPath+ "/" + 'modeloLBPHFace.xml')
-    
-    #popup pidiendo interaccion
-    # pregunta si el usuario tiene movil con capacidad foto
-    ca=messagebox.askquestion(title= "Recon_Facial", message="Tines Pc con camara apta para tomar foto/video")
+
+    # Popup pidiendo interacci贸n
+    # Pregunta si el usuario tiene m贸vil con capacidad foto
+    ca = messagebox.askquestion(title="Recon_Facial", message="Tines Pc con c谩mara apta para tomar foto/video")
     print (ca)
-    
+
     if (ca=="yes"):
-       #En la siguiente l韓ea se realiza un video en directo
-       cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+       # En la siguiente l铆nea se realiza un video en directo
+       cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     else:
-      ca1=messagebox.askquestion(title= "Recon_Facial", message="Tienes un movil con bluetooth activo y emparejado con tu PC con capacidad para tomar un video")
-      if (ca1=="yes"):
-          #En la siguiente l韓ea se lee  un video almacenado para hacer pruebas
-          cap = cv2.VideoCapture(dataPath+ "/" + 'Video.mp4')
-            
-      else:
-          #mecanismo de lock END
-         #-----------------------
-         lock.lockOUT("Reco_Facial")
-         key_size = 0
-         result =(NULL, key_size)
-         print ("result:",result)
-         return result
-         
+        ca1 = messagebox.askquestion(title="Recon_Facial", message="Tienes un m贸vil con bluetooth activo y emparejado con tu PC con capacidad para tomar un video")
+        if (ca1=="yes"):
+            # En la siguiente l铆nea se lee un video almacenado para hacer pruebas
+            cap = cv2.VideoCapture(dataPath + "/" + 'Video.mp4')
+        else:
+            # Mecanismo de lock END
+            #----------------------
+            lock.lockOUT("Reco_Facial")
+            key_size = 0
+            result =(NULL, key_size)
+            print ("result:",result)
+            return result
+
 
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-    print("MODELO LEIDO")
-    
+    print("MODELO LE脥DO")
+
     if True:
-        res=0
+        res = 0
         ret,frame = cap.read()
-        if ret == False: return 0 
+        if ret == False: return 0
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow('gray',gray)
+        #cv2.imshow('gray', gray)
         auxFrame = gray.copy()
-        faces = faceClassif.detectMultiScale(gray,1.3,5)
+        faces = faceClassif.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             rostro = auxFrame[y:y+h,x:x+w]
-            rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+            rostro = cv2.resize(rostro, (150,150), interpolation=cv2.INTER_CUBIC)
             result = face_recognizer.predict(rostro)
-            cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+            cv2.putText(frame, '{}'.format(result), (x,y-5), 1, 1.3, (255,255,0), 1, cv2.LINE_AA)
             print(result)
-        
+
             # LBPHFace
             if result[1] > 0 and result[1] <= 70:
-               cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-               cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-               cv2.imshow('frame',frame)
+               cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x,y-25), 2, 1.1, (0,255,0), 1, cv2.LINE_AA)
+               cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+               cv2.imshow('frame', frame)
                cv2.waitKey(0)
-               res= result[1]
-               #print (resultado)
+               res = result[1]
+               #print(resultado)
             elif result[1] <= 0 or result[1] > 70:
-                cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-                cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-                cv2.imshow('frame',frame)
-                cv2.waitKey(0)              
-                res= result[1]
-             
+                cv2.putText(frame, 'Desconocido', (x,y-20), 2, 0.8, (0,0,255), 1, cv2.LINE_AA)
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
+                cv2.imshow('frame', frame)
+                cv2.waitKey(0)
+                res = result[1]
+
     cv2.destroyAllWindows()
     cap.release()
 
 
-    #mecanismo de lock END
-    #-----------------------
+    # Mecanismo de lock END
+    #----------------------
     lock.lockOUT("Reco_Facial")
 
-
-    #construccion de la respuesta
-    if res <= 0:
-        resp= 0
-    elif res > 0 and res <= 70:
-        resp= 1
-    elif res> 70 and res<= 75:
-        resp= 2
-    elif res> 75 and res<= 80:
-        resp= 3
-    elif res> 80 and res<= 85:
-        resp= 4
-    elif res> 85 and res<= 90:
-        resp= 5
-    elif res> 90 and res<= 95:
-        resp= 6
-    elif res> 95 and res<= 100:
-        resp= 7
-    elif res> 100 and res<= 105:
-        resp= 8
-    elif res> 105 and res<= 110:
-        resp= 9
-    elif res> 110 and res<= 115:
-        resp= 10
-    elif res> 115 and res<= 120:
-        resp= 11
-    elif res> 120:
-        resp= 12
+    # Construcci贸n de la respuesta
+    if res<=0:                  resp = 0
+    elif res>0 and res<=70:     resp = 1
+    elif res>70 and res<=75:    resp = 2
+    elif res>75 and res<=80:    resp = 3
+    elif res>80 and res<=85:    resp = 4
+    elif res>85 and res<=90:    resp = 5
+    elif res>90 and res<=95:    resp = 6
+    elif res>95 and res<=100:   resp = 7
+    elif res>100 and res<=105:  resp = 8
+    elif res>105 and res<=110:  resp = 9
+    elif res>110 and res<=115:  resp = 10
+    elif res>115 and res<=120:  resp = 11
+    elif res>120:               resp = 12
 
 
-
-    cad="%d"%(resp)
-    key = bytes(cad,'utf-8')
+    cad = "%d"%(resp)
+    key = bytes(cad, 'utf-8')
     key_size = len(key)
-    result =(key, key_size)
-    print ("result:",result)
+    result = (key, key_size)
+    print("result:", result)
     return result
-    
+
 
 if __name__ == "__main__":
-    midict={}
+    midict = {}
     print(init(midict))
     #executeChallenge()
--- crear_BD.py
+++ crear_BD.py
@@ -1,4 +1,3 @@
-
 import cv2
 import os
 import imutils
@@ -7,77 +6,82 @@ import tkinter as tk
 from tkinter import messagebox
 import numpy as np
 
-#Se muestra una GUI para que el usuario entre su nombre y apellidos para identificalo
-ventana= tk.Tk()
+
+
+
+############### FASE DE OBTENCIÓN DE DATOS ###############
+
+# Se muestra una GUI para que el usuario entre su nombre y apellidos para identificalo
+ventana = tk.Tk()
 ventana.geometry('400x100')
-tk.Label(ventana,text='Entre nombre y apellidos', font = "Calibri 20").pack()
+tk.Label(ventana, text='Entre nombre y apellidos', font="Calibri 20").pack()
 
-nombre=tk.StringVar()
-nomb=tk.Entry(ventana, textvariable=nombre, font = "Calibri 12", width = 40).pack()
+nombre = tk.StringVar()
+nomb = tk.Entry(ventana, textvariable=nombre, font="Calibri 12", width=40).pack()
 
-#Se obtiene el nombre entrado por el usuario en personName
-# para crear la carpeta de imagenes para entrenar el modelo
+# Se obtiene el nombre entrado por el usuario en personName
+# para crear la carpeta de imágenes para entrenar el modelo
 def obtTex():
     personName = str(nombre.get())
-    print (personName)
-    return (personName)
+    print(personName)
+    return personName
 
 def cerrar():
     ventana.destroy()
 
-#Bot�n para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
-button1 = tk.Button(ventana, text = "OK", command = cerrar).pack(side= tk.BOTTOM)
+# Botón para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
+button1 = tk.Button(ventana, text="OK", command=cerrar).pack(side=tk.BOTTOM)
 ventana.mainloop()
 
-#Se crea una carpeta con el nombre del usuario a reconocer, 
-#esta se crear� dentro de la carpeta Data que ya se hab�a creado previamente de forma manual. 
-# Finalmente personPath ser� la ruta completa.
 
-d_Path=os.environ['SECUREMIRROR_CAPTURES']
-dataPath = d_Path + '/' + 'Data' #Cambia a la ruta donde hayas almacenado Data
+# Se crea una carpeta con el nombre del usuario a reconocer,
+# esta se creará dentro de la carpeta Data que ya se había creado previamente de forma manual.
+# Finalmente personPath será la ruta completa.
+d_Path = os.environ['SECUREMIRROR_CAPTURES']
+dataPath = d_Path + '/' + 'Data'        # Cambia a la ruta donde hayas almacenado Data
 personPath = dataPath + '/' + obtTex()
 
 if not os.path.exists(personPath):
-    print('Carpeta creada: ',personPath)
     os.makedirs(personPath)
+    print('Carpeta creada:', personPath)
 
-ca=messagebox.askquestion(title= "Recon_Facial", message="Tines Pc con camara apta para tomar foto/video")
-print (ca)
+ca = messagebox.askquestion(title="Recon_Facial", message="Tines Pc con cámara apta para tomar foto/video")
+print(ca)
 if (ca=="yes"):
-    #En la siguiente l�nea se realiza un video en directo
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    # En la siguiente línea se realiza un video en directo
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 else:
-    ca1=messagebox.askquestion(title= "Recon_Facial", message="Tienes un movil con bluetooth activo y emparejado con tu PC con capacidad para tomar video")
+    ca1 = messagebox.askquestion(title="Recon_Facial", message="Tienes un móvil con bluetooth activo y emparejado con tu PC con capacidad para tomar video")
     if (ca1=="yes"):
-        #En la siguiente l�nea se lee  un video almacenado para hacer pruebas
-        cap = cv2.VideoCapture(d_Path+ "/" + 'Video.mp4')
-   
-    
-   
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+        # En la siguiente línea se lee un video almacenado para hacer pruebas
+        cap = cv2.VideoCapture(d_Path + "/" + 'Video.mp4')
+
+
+# Procesamiento del video (capturado o almacenado)
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 count = 0
 while True:
-    
     ret, frame = cap.read()
     if ret == False: break
-    frame =  imutils.resize(frame, width=640)
+    frame = imutils.resize(frame, width=640)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     auxFrame = frame.copy()
-    faces = faceClassif.detectMultiScale(gray,1.3,5)
+    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
         rostro = auxFrame[y:y+h,x:x+w]
-        rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count),rostro)
+        rostro = cv2.resize(rostro, (150,150), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count), rostro)
         count = count + 1
-    cv2.imshow('frame',frame)
-    k =  cv2.waitKey(1)
+    cv2.imshow('frame', frame)
+    k = cv2.waitKey(1)
     if k == 27 or count >= 300:
         break
 cap.release()
 cv2.destroyAllWindows()
 
-#******* FASE DE ENTRENAMIENTO**************************
+
+############### FASE DE ENTRENAMIENTO ###############
 peopleList = os.listdir(dataPath)
 print('Lista de personas: ', peopleList)
 
@@ -87,22 +91,22 @@ label = 0
 
 for nameDir in peopleList:
     personPath = dataPath + '/' + nameDir
-    print('Leyendo las imagenes')
+    print('Leyendo las imágenes')
 
     for fileName in os.listdir(personPath):
         #print('Rostros: ', nameDir + '/' + fileName)
         labels.append(label)
-        facesData.append(cv2.imread(personPath+'/'+fileName,0))
-        image = cv2.imread(personPath+'/'+fileName,0)
-        #cv2.imshow('image',image)
+        facesData.append(cv2.imread(personPath + '/' + fileName, 0))
+        image = cv2.imread(personPath + '/' + fileName, 0)
+        #cv2.imshow('image', image)
         #cv2.waitKey(10)
     label = label + 1
-#print('labels= ',labels)
-#print('Numero de etiquetas 0: ',np.count_nonzero(np.array(labels)==0))
-#print('Numero de etiquetas 1: ',np.count_nonzero(np.array(labels)==1))
-#print('Numero de etiquetas 2: ',np.count_nonzero(np.array(labels)==2))
+#print('labels=', labels)
+#print('Número de etiquetas 0: ', np.count_nonzero(np.array(labels)==0))
+#print('Número de etiquetas 1: ', np.count_nonzero(np.array(labels)==1))
+#print('Número de etiquetas 2: ', np.count_nonzero(np.array(labels)==2))
 
-#Entrenando el modelo
+# Entrenando el modelo
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 # Entrenando el reconocedor de rostros
 print("Entrenando...")
