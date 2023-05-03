30da74ef8346d80f706fcf1299649f06da8b4bad
Sergio Molina Cardín <sergio.molina_cardin.ext@nokia.com>
2023-05-03 13:21:45 +0200

    Improved prints. Increased robustness in error checking. Ommited challenge execution inside init() function. Reformatted lock.py.

--- Reco_Facial.py
+++ Reco_Facial.py
@@ -17,14 +17,13 @@ import lock
 # ------------------
 props_dict = {}
 
-
 def init(props):
     global props_dict
-    print("Python: Enter in init")
+    print("CHALLENGE_RECO_FACIAL --> Enter in init")
 
     # Props es un diccionario
     props_dict = props
-    resultado = executeChallenge()
+    resultado = ('1', 1) #executeChallenge()
     if (resultado[1]>0):
         return 0
     else:
@@ -33,11 +32,11 @@ def init(props):
 
 
 def executeChallenge():
-    print("Starting execute")
+    print("CHALLENGE_RECO_FACIAL --> Starting execute")
     #for key in os.environ: print(key, ':', os.environ[key])
     dataPath = os.environ['SECUREMIRROR_CAPTURES']
 
-    print ("storage folder is :",dataPath)
+    print("CHALLENGE_RECO_FACIAL --> Storage folder is:", dataPath)
 
     # Mecanismo de lock BEGIN
     # -----------------------
@@ -46,21 +45,21 @@ def executeChallenge():
 
     #dataPath = 'B:/Doctorado/Challenges/Data' #Cambia a la ruta donde hayas almacenado Data
     imagePaths = os.listdir(dataPath + "/" + "Data")
-    print('imagePaths=', imagePaths)
+    print("CHALLENGE_RECO_FACIAL --> imagePaths=", imagePaths)
 
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
     # Leyendo el modelo
-    face_recognizer.read(dataPath+ "/" + 'modeloLBPHFace.xml')
+    face_recognizer.read(dataPath + "/" + 'modeloLBPHFace.xml')
 
     # Popup pidiendo interacción
     # Pregunta si el usuario tiene móvil con capacidad foto
     ca = messagebox.askquestion(title="Recon_Facial", message="Tines Pc con cámara apta para tomar foto/video")
-    print (ca)
+    print("CHALLENGE_RECO_FACIAL --> Camera: ", ca)
 
     if (ca=="yes"):
-       # En la siguiente línea se realiza un video en directo
-       cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        # En la siguiente línea se realiza un video en directo
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     else:
         ca1 = messagebox.askquestion(title="Recon_Facial", message="Tienes un móvil con bluetooth activo y emparejado con tu PC con capacidad para tomar un video")
         if (ca1=="yes"):
@@ -71,46 +70,61 @@ def executeChallenge():
             #----------------------
             lock.lockOUT("Reco_Facial")
             key_size = 0
-            result =(NULL, key_size)
-            print ("result:",result)
+            result = (NULL, key_size)
+            print("CHALLENGE_RECO_FACIAL --> result:", result)
             return result
 
 
-    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-    print("MODELO LEÍDO")
+    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    print("CHALLENGE_RECO_FACIAL --> Model has been read")
 
-    if True:
-        res = 0
+    res = 0
+
+    try:
         ret,frame = cap.read()
-        if ret == False: return 0
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow('gray', gray)
-        auxFrame = gray.copy()
-        faces = faceClassif.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            rostro = auxFrame[y:y+h,x:x+w]
-            rostro = cv2.resize(rostro, (150,150), interpolation=cv2.INTER_CUBIC)
-            result = face_recognizer.predict(rostro)
-            cv2.putText(frame, '{}'.format(result), (x,y-5), 1, 1.3, (255,255,0), 1, cv2.LINE_AA)
-            print(result)
+    except:
+        print("CHALLENGE_RECO_FACIAL --> Error: cannot get frame from camera")
+        result = (NULL, key_size)
+        print("CHALLENGE_RECO_FACIAL --> result:", result)
+        return result
 
-            # LBPHFace
-            if result[1] > 0 and result[1] <= 70:
-               cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x,y-25), 2, 1.1, (0,255,0), 1, cv2.LINE_AA)
-               cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-               cv2.imshow('frame', frame)
-               cv2.waitKey(0)
-               res = result[1]
-               #print(resultado)
-            elif result[1] <= 0 or result[1] > 70:
-                cv2.putText(frame, 'Desconocido', (x,y-20), 2, 0.8, (0,0,255), 1, cv2.LINE_AA)
-                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
-                cv2.imshow('frame', frame)
-                cv2.waitKey(0)
-                res = result[1]
+    if ret == False:
+        cap.release()
+        cv2.destroyAllWindows()
+        lock.lockOUT("Reco_Facial")
+        key_size = 0
+        result = (NULL, key_size)
+        print("CHALLENGE_RECO_FACIAL --> result:", result)
+        return result
 
-    cv2.destroyAllWindows()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('gray', gray)
+    #cv2.waitKey(0)
+    auxFrame = gray.copy()
+    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        rostro = auxFrame[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro, (150,150), interpolation=cv2.INTER_CUBIC)
+        result = face_recognizer.predict(rostro)
+        cv2.putText(frame, '{}'.format(result), (x,y-5), 1, 1.3, (255,255,0), 1, cv2.LINE_AA)
+        print("CHALLENGE_RECO_FACIAL --> result:", result)
+
+        # LBPHFace
+        if result[1] > 0 and result[1] <= 70:
+            cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x,y-25), 2, 1.1, (0,255,0), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+            cv2.imshow('frame', frame)
+            cv2.waitKey(0)
+            res = result[1]
+        elif result[1] <= 0 or result[1] > 70:
+            cv2.putText(frame, 'Desconocido', (x,y-20), 2, 0.8, (0,0,255), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
+            cv2.imshow('frame', frame)
+            cv2.waitKey(0)
+            res = result[1]
+
     cap.release()
+    cv2.destroyAllWindows()
 
 
     # Mecanismo de lock END
@@ -132,16 +146,15 @@ def executeChallenge():
     elif res>115 and res<=120:  resp = 11
     elif res>120:               resp = 12
 
-
     cad = "%d"%(resp)
     key = bytes(cad, 'utf-8')
     key_size = len(key)
     result = (key, key_size)
-    print("result:", result)
+    print("CHALLENGE_RECO_FACIAL --> result:", result)
     return result
 
 
 if __name__ == "__main__":
     midict = {}
     print(init(midict))
-    #executeChallenge()
+    print(executeChallenge())
--- lock.py
+++ lock.py
@@ -6,27 +6,27 @@ import time
 import fnmatch
 
 def searchLock(challenge_name):
-    folder=os.environ['SECUREMIRROR_CAPTURES']
+    folder = os.environ['SECUREMIRROR_CAPTURES']
     while os.path.isdir(folder)==False:
-        print (" challenge : SECUREMIRROR_CAPTURES environment var not valid")
+        print(" challenge : SECUREMIRROR_CAPTURES environment variable not valid")
         return False
         time.sleep(60) # check and sleep forever
         
-    #si llegamos aqui, el directorio existe
+    # Si llegamos aqui, el directorio existe
         
     for file in os.listdir(folder):
         if fnmatch.fnmatch(file,'lock_*'):
-            print ("file found:", file)
-            #hemos encontrado un fichero que cumple
-            # si es nuestro challenge, ignoramos
-            if file=="lock_"+challenge_name:
-                print ("el lock es del mismo challenge. lo ignoramos")
-                continue
-            creation_date=os.path.getctime(folder+"/"+file)
-            now= time.time()
-            if (now>creation_date+300): # 5 minutos es viejo
-                # es viejo
-                print (file, "is old")
+            print("file found:", file)
+            # Hemos encontrado un fichero que cumple
+            # Si es nuestro challenge, ignoramos
+            # if file=="lock_" + challenge_name:
+            #     print("El lock es de este mismo challenge: lo ignoramos")
+            #     continue
+            creation_date = os.path.getctime(folder + "/" + file)
+            now = time.time()
+            if (now > creation_date+300): # 5 minutos es viejo
+                # Es viejo
+                print(file, "is old")
                 continue
             else:
                 return False
@@ -34,27 +34,27 @@ def searchLock(challenge_name):
     
 def lockIN(challenge_name):
     
-    #mientras exista un fichero lock* se queda en bucle
+    # Mientras exista un fichero lock* se queda en bucle
     while True:
-        search=searchLock(challenge_name)
+        search = searchLock(challenge_name)
         if (search==True):
             break
         else:
             time.sleep(5)
             
-    # si llegamos aquies que no hay lock o lo que hay es viejo
-    # tambien tenemos garantizado si llegamos aqui que folder existe
+    # Si llegamos aquies que no hay lock o lo que hay es viejo
+    # Tambien tenemos garantizado si llegamos aqui que folder existe
     
-    folder=os.environ['SECUREMIRROR_CAPTURES']
-    #borramos un posible fichero viejo nuestro
-    if os.path.exists(folder+"/"+"lock_"+challenge_name):
-        os.remove(folder+"/"+"lock_"+challenge_name)
-    #creamos el nuevo
-    Path(folder+"/"+"lock_"+challenge_name).touch()
+    folder = os.environ['SECUREMIRROR_CAPTURES']
+    # Borramos un posible fichero viejo nuestro
+    if os.path.exists(folder + "/" + "lock_" + challenge_name):
+        os.remove(folder + "/" + "lock_" + challenge_name)
+    # Creamos el nuevo
+    Path(folder + "/" + "lock_" + challenge_name).touch()
     
 
 def lockOUT(challenge_name):
-    folder=os.environ['SECUREMIRROR_CAPTURES']
-    if os.path.exists(folder+"/"+"lock_"+challenge_name):
-        os.remove(folder+"/"+"lock_"+challenge_name)
+    folder = os.environ['SECUREMIRROR_CAPTURES']
+    if os.path.exists(folder + "/" + "lock_" + challenge_name):
+        os.remove(folder + "/" + "lock_" + challenge_name)
     
